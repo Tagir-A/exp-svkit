835fdbed35410215f9a3585998c576120a17597c
Tagir Almasov <tccup1@gmail.com>
2021-05-22 17:29:20 +0000

    init kubachi page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,6 @@
-<script>
-	import Test from '../markdown/test.svx'
-</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Truing out sveltekit in the wild</p>
 
-<Test />
+<a sveltekit:prefetch href="/kubachi">About Kubachi</a>
---
+++ src/routes/kubachi.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Kubachi from '../markdown/kubachi.svx'
+</script>
+
+<Kubachi />
