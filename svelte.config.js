d129140b03d0fa0a38633f54ac1c15faa5825e87
Tagir Almasov <tccup1@gmail.com>
2021-05-22 17:01:58 +0000

    install markdown plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svx', '.svelte'],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), mdsvex()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
